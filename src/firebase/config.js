82ec55014dcd25bbbd5ba45c3cf3d4e33f3702e8
Robin <robinsncorvids@gmail.com>
2023-03-14 10:38:42 -0700

    added backend

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD-Sb20HkrwuRtSG0uKieGGUB6kkmFrZgc",
+    authDomain: "firegram-tutorial-projec-b9464.firebaseapp.com",
+    projectId: "firegram-tutorial-projec-b9464",
+    storageBucket: "firegram-tutorial-projec-b9464.appspot.com",
+    messagingSenderId: "179369573587",
+    appId: "1:179369573587:web:fbea52654769c19ce82826"
+  };
+  
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
